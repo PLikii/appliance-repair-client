cd6286badce7324076ac44f11a60b068499f46a8
PLikii <marioua672@gmail.com>
2023-02-08 08:33:04 +0200

    add isLoding to CreateOrder

--- src/pages/castomer/CreateOrder.jsx
+++ src/pages/castomer/CreateOrder.jsx
@@ -3,8 +3,12 @@ import { useFormik } from "formik";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import { useState } from "react";
+import { RxUpdate } from "react-icons/rx";
 
 function CreateOrder() {
+  const [isLoading, setIsLoading] = useState(false);
+
   const navigate = useNavigate();
   const formik = useFormik({
     initialValues: {
@@ -36,16 +40,25 @@ function CreateOrder() {
         ],
       };
 
-      if (data.technic_type === "") return toast.error("Виберіть тип техніки");
-      if (data.customer_name === "")
+      if (data.technic_type === "") {
+        return toast.error("Виберіть тип техніки");
+      }
+      if (data.customer_name === "") {
         return toast.error("Ви не увели ім'я та прізфище");
-      if (data.customer_address === "") return toast.error("Ви не увели адрес");
-      if (data.customer_namber === "")
+      }
+      if (data.customer_address === "") {
+        return toast.error("Ви не увели адрес");
+      }
+      if (data.customer_namber === "") {
         return toast.error("Ви не увели мобільний номер");
-      if (data.customer_namber.length < 10)
+      }
+      if (data.customer_namber.length < 10) {
         return toast.error("Мобільний номер занадто короткий");
+      }
       if (data.description === "")
         return toast.error("Ви не увели опис проблеми");
+
+      setIsLoading(true);
 
       await axios
         .post("http://127.0.0.1:5000/order", {
@@ -57,11 +70,31 @@ function CreateOrder() {
         })
         .catch((e) => {
           return toast.error(e);
+        })
+        .finally(() => {
+          setIsLoading(false);
+          toast.dismiss();
         });
     },
   });
+
   return (
-    <div className="space-t-10 ">
+    <div>
+      {isLoading ? (
+        <div className=" fixed w-full h-screen bg-dark/60  ">
+          <div className=" flex justify-center mt-16">
+            <div className=" space-y-4">
+              <div className=" flex justify-center ">
+                <RxUpdate size={30} className=" animate-spin" fill="white" />
+              </div>
+              <div>Замавлення створеється</div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+
       <div className="py-7 px-10 lg:flex items-center justify-between space-y-10 lg:space-y-0">
         <div className="space-y-3 font-bold ">
           <div className="text-4xl">appliance repair</div>
@@ -158,12 +191,14 @@ function CreateOrder() {
             </div>
 
             <div>
-              <button
-                className=" py-4 px-7 text-lg border-4 border-blue border-double rounded-lg transition duration-300 hover:scale-105"
-                type="submit"
-              >
-                Замовити
-              </button>
+              <div className={`${isLoading ? "hidden " : "inline"}`}>
+                <button
+                  className=" py-4 px-7 text-lg  border-4 border-blue border-double rounded-lg transition duration-300 hover:scale-105"
+                  type="submit"
+                >
+                  Замовити
+                </button>
+              </div>
             </div>
           </form>
         </div>
